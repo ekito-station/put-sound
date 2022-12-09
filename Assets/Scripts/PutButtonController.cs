1b225c71b4e2a2f6143ec0bb8bf49c7b06be3691
ekito-line <ekito.line@gmail.com>
2022-12-09 16:07:13 +0900

    recreated the repo

---
+++ Assets/Scripts/PutButtonController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutButtonController : MonoBehaviour
+{
+    public GameObject arCamera;
+    public GameObject cubePrefab;
+    public GameObject markerPrefab;
+
+    public float front;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickPutButton()
+    {
+        // Vector3 cameraPosition = arCamera.transform.position; // AR Cameraの座標を取得
+
+        Transform camTran = arCamera.transform;
+        Vector3 curPos = camTran.position + front * camTran.forward;
+
+        // GameObject markerObject = Instantiate(markerPrefab, cameraPosition, Quaternion.identity);
+        GameObject markerObject = Instantiate(markerPrefab, curPos, Quaternion.identity);
+        Debug.Log("Placed a marker.");
+    }
+}
